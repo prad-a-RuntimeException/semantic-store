37b04508e3abaf7bbf10ce51a70f64536c0f0f36
pradheep <rpradhee@thoughtworks.com>
2017-02-03 10:04:11 -0800

    Adding spark graphframes to the project.

--- src/main/scala/recipestore/graph/PropertyGraphFactory.scala
+++ src/main/scala/recipestore/graph/PropertyGraphFactory.scala
@@ -1,6 +1,14 @@
 package recipestore.graph
 
+import org.apache.jena.rdf.model.Resource
+import org.graphframes.GraphFrame
+
 class PropertyGraphFactory {
+
+
+  def createGraph(resource: Resource): GraphFrame = {
+    null
+  }
 
 
 }
